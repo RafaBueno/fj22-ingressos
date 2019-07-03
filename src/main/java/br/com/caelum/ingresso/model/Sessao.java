4720a43bea36dd41d1eb06ec02b03f304ef1f691
Rafael Bueno <rafael_wek@hotmail.com>
2019-07-02 21:30:47 -0300

    cadsatrando sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,7 +2,39 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
+
+	private LocalTime horario;
+	
+	@ManyToOne
+	private Sala sala;
+	
+	@ManyToOne
+	private Filme filme;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	/**
+	* @deprecated hibernate only
+	*/
+	
+	public Sessao() {
+	}
+
+	public Sessao(LocalTime horario, Filme filme, Sala sala) {
+		this.horario = horario;
+		this.filme = filme;
+		this.sala = sala;
+	}
 	
 	public LocalTime getHorario() {
 		return horario;
@@ -34,16 +66,5 @@ public class Sessao {
 
 	public void setId(Integer id) {
 		this.id = id;
-	}
-
-	private LocalTime horario;
-	private Sala sala;
-	private Filme filme;
-	private Integer id;
-
-	public Sessao(LocalTime horario, Filme filme, Sala sala) {
-		this.horario = horario;
-		this.filme = filme;
-		this.sala = sala;
 	}
 }
