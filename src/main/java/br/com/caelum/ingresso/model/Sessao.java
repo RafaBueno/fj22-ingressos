10decc6df9fe2097175bee3e8e0a03ef11b51b1c
Rafael Bueno <rafael_wek@hotmail.com>
2019-07-04 20:52:55 -0300

    efetuando teste

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -12,21 +14,23 @@ import javax.persistence.ManyToOne;
 public class Sessao {
 
 	private LocalTime horario;
-	
+
 	@ManyToOne
 	private Sala sala;
-	
+
 	@ManyToOne
 	private Filme filme;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
+	private BigDecimal preco = BigDecimal.ZERO;
+
 	/**
-	* @deprecated hibernate only
-	*/
-	
+	 * @deprecated hibernate only
+	 */
+
 	public Sessao() {
 	}
 
@@ -34,8 +38,17 @@ public class Sessao {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
-	
+
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
 	public LocalTime getHorario() {
 		return horario;
 	}
